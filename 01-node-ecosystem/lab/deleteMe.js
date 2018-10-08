5d1fb6c386f02955431a228a8c20ee5b5cb555d3
James McDaniel <icathaid@gmail.com>
2018-10-08 12:20:08 -0700

    added reading

---
+++ 01-node-ecosystem/lab/deleteMe.js
@@ -0,0 +1,22 @@
+describe('Greet Module', () => {
+    it('describes what is being tested', () => {
+
+        // run some commands
+
+        expect('something').toBe('the expected result');
+
+    });
+
+});
+
+module.exports = {
+    addFunction = (a, b) => {
+
+    },
+
+};
+
+
+if (NaN(a)) {
+
+}
